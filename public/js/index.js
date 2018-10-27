d317daadf27ab959999a35735ba7b31e5758ec72
Amir Taghinia <amirstep@Amirs-iMac.home>
2018-10-26 20:47:23 -0400

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+            
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'jen@exmaple.com',
+        text: 'Hey, what up yo'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (msg) { // this is the listener for a custom event that we titled 'newEmail'
+    console.log('newMessage', msg);
+});
--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+
+    socket.emit('newMessage', {
+        from: 'mike@example.com',
+        text: 'Hey, what\'s going on?',
+        createAt: 123
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage: ', newMessage);
+    });
+
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
